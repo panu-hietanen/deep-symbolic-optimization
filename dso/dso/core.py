53bac8a677f31a108541cfc1e53da02d28b92677
panu-hietanen <panu.hietanen@gmail.com>
2025-02-14 18:05:18 +0000

    Separated trainer classes into synchronised parallel and single thread

--- dso/dso/core.py
+++ dso/dso/core.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 import commentjson as json
 
 from dso.task import set_task
-from dso.train import Trainer
+from dso.train import SingleTrainer, SyncTrainer
 from dso.checkpoint import Checkpoint
 from dso.train_stats import StatsLogger
 from dso.prior import make_prior
@@ -206,13 +206,22 @@ class DeepSymbolicOptimizer():
         return state_manager
 
     def make_trainer(self):
-        trainer = Trainer(self.sess,
-                          self.policy,
-                          self.policy_optimizer,
-                          self.gp_controller,
-                          self.logger,
-                          self.pool,
-                          **self.config_training)
+        if self.sync:
+            trainer = SyncTrainer(self.sess,
+                            self.policy,
+                            self.policy_optimizer,
+                            self.gp_controller,
+                            self.logger,
+                            self.pool,
+                            **self.config_training)
+        else:
+            trainer = SingleTrainer(self.sess,
+                            self.policy,
+                            self.policy_optimizer,
+                            self.gp_controller,
+                            self.logger,
+                            self.pool,
+                            **self.config_training)
         return trainer
 
     def make_logger(self):
@@ -266,7 +275,7 @@ class DeepSymbolicOptimizer():
         pool = None
         n_cores_batch = self.config_training.get("n_cores_batch")
         n_cores_task = self.config_training.get("n_cores_task")
-        self.sync = self.config_training.get("sync")
+        self.sync = self.config_training.pop("sync")
 
         if n_cores_task is None:
             raise ValueError("'n_cores_task' variable has not been set in config.")
