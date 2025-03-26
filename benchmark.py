542b2b7224c03f4d258b9d07ffa3714a4e503ecb
panu-hietanen <panu.hietanen@gmail.com>
2025-03-26 16:37:47 +0000

    Avoid repeated code

--- benchmark.py
+++ benchmark.py
@@ -17,6 +17,8 @@ from dso.logeval import LogEval
 from dso.config import load_config
 from dso.utils import safe_update_summary
 
+from run_utils import print_summary, clean_config, run_experiment
+
 PARAM_MAPPING = {
     'lr': 'learning_rate',
     'ew': 'entropy_weight',
@@ -40,159 +42,6 @@ CONFIG_MAPPING = {
     'epsilon': 'training',
     'alpha_train': 'training',
 }
-
-def train_dso(config, model):
-    """Trains DSO and returns dict of reward, expression, and traversal"""
-
-    print("\n== TRAINING SEED {} START ============".format(config["experiment"]["seed"]))
-
-    # For some reason, for the control task, the environment needs to be instantiated
-    # before creating the pool. Otherwise, gym.make() hangs during the pool initializer
-    ''' # Removed gym error as not using control task
-    if config["task"]["task_type"] == "control" and config["training"]["n_cores_batch"] > 1:
-        import gym
-        import dso.task.control # Registers custom and third-party environments
-        gym.make(config["task"]["env"])
-    '''
-
-    # Train the model
-    start = time.time()
-    result = model.train()
-    result["t"] = time.time() - start
-    result.pop("program")
-
-    save_path = model.config_experiment["save_path"]
-    summary_path = os.path.join(save_path, "summary.csv")
-
-    print("== TRAINING SEED {} END ==============".format(config["experiment"]["seed"]))
-
-    return result, summary_path
-
-
-def print_summary(config, runs, messages):
-    text = '\n== EXPERIMENT SETUP START ===========\n'
-    text += 'Task type            : {}\n'.format(config["task"]["task_type"])
-    if config["task"]["task_type"] == "regression":
-        text += 'Dataset              : {}\n'.format(config["task"]["dataset"])
-    elif config["task"]["task_type"] == "control":
-        text += 'Environment          : {}\n'.format(config["task"]["env"])
-    text += 'Starting seed        : {}\n'.format(config["experiment"]["seed"])
-    text += 'Runs                 : {}\n'.format(runs)
-    if len(messages) > 0:
-        text += 'Additional context   :\n'
-        for message in messages:
-            text += "      {}\n".format(message)
-    text += '== EXPERIMENT SETUP END ============='
-    print(text)
-
-
-def clean_config(config_template="", runs=1, n_cores_task=1, seed=None, benchmark=None, exp_name=None, time=0):
-    """Runs DSO in parallel across multiple seeds using multiprocessing."""
-
-    messages = []
-
-    # Load the experiment config
-    config_template = config_template if config_template != "" else None
-    config = load_config(config_template)
-
-    # Overwrite named benchmark (for tasks that support them)
-    task_type = config["task"]["task_type"]
-    if benchmark is not None:
-        # For regression, --b overwrites config["task"]["dataset"]
-        if task_type == "regression":
-            config["task"]["dataset"] = benchmark
-        # For control, --b overwrites config["task"]["env"]
-        elif task_type == "control":
-            config["task"]["env"] = benchmark
-        else:
-            raise ValueError("--b is not supported for task {}.".format(task_type))
-
-    # Update save dir if provided
-    if exp_name is not None:
-        config["experiment"]["exp_name"] = exp_name
-
-    # Overwrite config seed, if specified
-    if seed is not None:
-        if config["experiment"]["seed"] is not None:
-            messages.append(
-                "INFO: Replacing config seed {} with command-line seed {}.".format(
-                    config["experiment"]["seed"], seed))
-        config["experiment"]["seed"] = seed
-
-    # Save starting seed and run command
-    config["experiment"]["starting_seed"] = config["experiment"]["seed"]
-    config["experiment"]["cmd"] = " ".join(sys.argv)
-
-    # Set timestamp once to be used by all workers
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H%M%S") + str(time)
-    config["experiment"]["timestamp"] = timestamp
-
-    # Fix incompatible configurations
-    if n_cores_task == -1:
-        n_cores_task = multiprocessing.cpu_count()
-    if n_cores_task > runs and not config["training"]["sync"]:
-        messages.append(
-                "INFO: Setting 'n_cores_task' to {} because there are only {} runs.".format(
-                    runs, runs))
-        n_cores_task = runs
-    if n_cores_task == 1 and config["training"]["sync"]:
-        messages.append(
-            "INFO: Setting 'sync' to False as there is only one core being used"
-        )
-        config["training"]["sync"] = False
-    if config["training"]["verbose"] and n_cores_task > 1:
-        messages.append(
-                "INFO: Setting 'verbose' to False for parallelized run.")
-        config["training"]["verbose"] = False
-    if config["training"]["n_cores_batch"] != 1 and n_cores_task > 1:
-        messages.append(
-                "INFO: Setting 'n_cores_batch' to 1 to avoid nested child processes.")
-        config["training"]["n_cores_batch"] = 1
-    if config["gp_meld"]["run_gp_meld"] and n_cores_task > 1 and runs > 1:
-        messages.append(
-                "INFO: Setting 'parallel_eval' to 'False' as we are already parallelizing.")
-        config["gp_meld"]["parallel_eval"] = False
-
-    if config["training"]["sync"]:
-        messages.append(
-            "INFO: Logging will be diminished for synchronous run."
-        )
-
-    # Save n_cores_task to config
-    config["training"]["n_cores_task"] = n_cores_task
-
-    return config, runs, n_cores_task, messages
-
-def run_experiment(config, runs, n_cores_task, model):
-    # Generate configs (with incremented seeds) for each run
-    configs = [deepcopy(config) for _ in range(runs)]
-    for i, config in enumerate(configs):
-        config["experiment"]["seed"] += i
-
-    # Farm out the work
-    if n_cores_task > 1 and not config["training"]["sync"]:
-        pool = multiprocessing.Pool(n_cores_task)
-        for i, (result, summary_path) in enumerate(pool.imap_unordered(train_dso, configs)):
-            if not safe_update_summary(summary_path, result):
-                print("Warning: Could not update summary stats at {}".format(summary_path))
-            print("INFO: Completed run {} of {} in {:.0f} s".format(i + 1, runs, result["t"]))
-    else:
-        for i, config in enumerate(configs):
-            result, summary_path = train_dso(config, model)
-            if not safe_update_summary(summary_path, result):
-                print("Warning: Could not update summary stats at {}".format(summary_path))
-            print("INFO: Completed run {} of {} in {:.0f} s".format(i + 1, runs, result["t"]))
-
-    # Evaluate the log files
-    print("\n== POST-PROCESS START =================")
-    log = LogEval(config_path=os.path.dirname(summary_path))
-    log.analyze_log(
-        show_count=config["postprocess"]["show_count"],
-        show_hof=config["logging"]["hof"] is not None and config["logging"]["hof"] > 0,
-        show_pf=config["logging"]["save_pareto_front"],
-        save_plots=config["postprocess"]["save_plots"])
-    print("== POST-PROCESS END ===================")
-    return summary_path
 
 def benchmark(config, benchmarks, runs=1, n_cores_task=1):
     summaries = []
@@ -271,10 +120,15 @@ def postprocess(summaries, timestamp, save_results=False):
         total_runs=("success", "count"),
         success_count=("success", "sum"),
         avg_samples = ("n_samples", "mean"),
+        avg_reward=("r", "mean"),
     ).reset_index()
 
     summary_df["failure_count"] = summary_df["total_runs"] - summary_df["success_count"]
     summary_df["success_rate"] = 100.0 * summary_df["success_rate"]
+
+    summary_df["std_reward"] = grouped["r"].std().values
+    summary_df["min_reward"] = grouped["r"].min().values
+    summary_df["max_reward"] = grouped["r"].max().values
 
     summary_df["std_time"] = grouped["t"].std().values
     summary_df["min_time"] = grouped["t"].min().values
@@ -320,8 +174,8 @@ def main(save_results=False, config_path='', runs=1, n_cores_task=1):
         raise ValueError(f'Error reading config file {config_path}: {e}')
 
     # Benchmarks
-    benchmarks = [f'Nguyen-{i}' for i in range(1,13)]
-    # benchmarks = ['Nguyen-1', 'Nguyen-2']
+    # benchmarks = [f'Nguyen-{i}' for i in range(1,13)]
+    benchmarks = ['Nguyen-12']
     print(f"INFO: RUNNING {len(benchmarks)} BENCHMARKS {runs} TIMES")
     benchmarks *= runs
 
@@ -335,7 +189,7 @@ def main(save_results=False, config_path='', runs=1, n_cores_task=1):
 if __name__ == "__main__":
     save_results = True
     config_path = '/homes/55/panu/4yp/deep-symbolic-optimization/dso/dso/config/config_regression.json'
-    runs = 2
-    n_cores_task = 2
+    runs = 10
+    n_cores_task = 5
     main(save_results, config_path, runs, n_cores_task)
 
