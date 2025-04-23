b38ad8517f7996a7dd8770c7077c154418287173
panu-hietanen <panu.hietanen@gmail.com>
2025-04-23 08:41:26 +0100

    Benchmark all expressions

--- benchmark.py
+++ benchmark.py
@@ -321,9 +321,9 @@ def main(save_results=False, config_path='', runs=1, n_cores_task=1, recovery_fi
         raise ValueError(f'Error reading config file {config_path}: {e}')
 
     # Benchmarks
-    # benchmarks = [f'Nguyen-{i}' for i in range(1,13)]
+    benchmarks = [f'Nguyen-{i}' for i in range(1,13)]
     # benchmarks = [f'Jin-{i}' for i in range(1,7)]
-    benchmarks = ['Nguyen-1', 'Nguyen-11']
+    # benchmarks = ['Nguyen-1', 'Nguyen-11']
     print(f"INFO: RUNNING {len(benchmarks)} BENCHMARKS {runs} TIMES")
     benchmarks *= runs
 
@@ -339,7 +339,7 @@ def main(save_results=False, config_path='', runs=1, n_cores_task=1, recovery_fi
 if __name__ == "__main__":
     save_results = True
     config_path = '/homes/55/panu/4yp/deep-symbolic-optimization/dso/dso/config/config_regression.json'
-    runs = 2
+    runs = 20
     n_cores_task = 5
     # recovery_files = ['./log_hypers/Jin-1_2025-04-09-1632350', './log_hypers/Jin-1_2025-04-09-1632355', './log_hypers/Jin-2_2025-04-09-1632351', './log_hypers/Jin-2_2025-04-09-1632356']
     recovery_files = None
